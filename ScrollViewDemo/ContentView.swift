d7662c38543e547633cdcf04edafe9299f4d5674
AnnaVas24 <95534449+AnnaVas24@users.noreply.github.com>
2023-07-07 15:57:53 +0300

    Initial Commit

---
+++ ScrollViewDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrollViewDemo
+//
+//  Created by Vasichko Anna on 07.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScrollViewDemo/ScrollViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewDemoApp.swift
+//  ScrollViewDemo
+//
+//  Created by Vasichko Anna on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
